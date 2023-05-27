bbaee68d8f55ca8b3fa7be5649fc399c2fe64197
Desmond <odakpo006@gmail.com>
2023-05-26 18:18:38 -0700

    changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,8 +4,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRouter = require('./routes/users')
+const paymentRouter = require('./routes/payments');
+const investmentRouter = require('./routes/investment');
+const referralRouter = require('./routes/referrals');
 const logger = require('morgan')
-const paymentModel = require('./model/paymentModel')
+const userModel = require('../src/model/userModel')
+
 
 
 
@@ -28,7 +32,10 @@ mongoose
 
 app.use(express.json());
 app.use(logger('dev'));
-app.use('/users', userRouter); 
+app.use('/users', userRouter);
+app.use('/payments', paymentRouter);
+app.use('/investment', investmentRouter);
+app.use('/referrals', referralRouter);
 
 
  
